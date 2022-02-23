df7bd8f26adffab99578f7a300be32684156d65e
Heinrich wolff <hcwolff94@gmail.com>
2022-02-23 15:16:30 +0100

    javascript på nav

---
+++ main.js
@@ -0,0 +1,62 @@
+const bog = document.getElementById("bog");
+
+function pop() {
+
+    bog.classList.add('animate__heartBeat')
+
+}
+
+const computer = document.getElementById("computer");
+
+function pop1() {
+
+    computer.classList.add('animate__heartBeat')
+
+}
+
+const skole = document.getElementById("skole");
+
+function pop2() {
+
+    skole.classList.add('animate__heartBeat')
+
+}
+const arrangement = document.getElementById("event");
+
+function pop3() {
+
+    arrangement.classList.add('animate__heartBeat')
+
+}
+
+const underholdning = document.getElementById("underholdning");
+
+function pop4() {
+
+    underholdning.classList.add('animate__heartBeat')
+
+}
+
+const nyhed = document.getElementById("nyhed");
+
+function pop5() {
+
+    nyhed.classList.add('animate__heartBeat')
+
+}
+
+const kontakt = document.getElementById("kontakt");
+
+function pop6() {
+
+    kontakt.classList.add('animate__heartBeat')
+
+}
+
+const info = document.getElementById("info");
+
+function pop7() {
+
+    info.classList.add('animate__heartBeat')
+
+}
